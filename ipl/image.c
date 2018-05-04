2a46e0048a6e46ae8b4e0727501d8aca7c02feab
LetsPlentendo <letsplentendo@gmail.com>
2018-05-04 19:53:26 +0200

    Make displaying image 70x faster & make image converter in python

--- ipl/image.c
+++ ipl/image.c
@@ -24,45 +24,19 @@
 #include "btn.h"
 
 int draw_image(gfx_con_t *con, char *ini_path) {
-	char lbuf[8];
-	char cbuf[8];
+	char lbuf[9];
 	FIL fp;
-	int x = 0;
-  int y = 0;
-
 	if (f_open(&fp, ini_path, FA_READ) != FR_OK) {
     gfx_con_setpos(con, 0, 0);
     gfx_printf(con, "File not found. Please make sure image.txt is located on the SD card.");
 		return 0;
   }
-	f_gets(lbuf, 8, &fp);
-	int width = strtoul(lbuf, NULL, 10);
-	unsigned int currColor;
-	char slbuf[width * 6];
-	while (f_gets(slbuf, width * 6 + 2, &fp)) {
-		for (int i = 1; i < width * 6 + 2; i += 6) {
-			substring(slbuf, cbuf, i, 6);
-			currColor = 0x00000000 + strtoul(cbuf, NULL, 16);
-			con->gfx_ctxt->fb[x + y * 768] = currColor;
-			x++;
-		}
-		x = 0;
-		y++;
-		u32 btn = btn_read();
-		if (btn & BTN_POWER) {
-			break;
-		}
-	}
-
+	f_gets(lbuf, 9, &fp);
+	//TODO: fix size in image converter, not here
+	int size = strtoul(lbuf, NULL, 10) * 5;
+	char slbuf[size + 1];
+	int bytesread = 0;
+	f_read(&fp, slbuf, size, &bytesread);
+	memcpy(con->gfx_ctxt->fb, slbuf + 1, size);
 	f_close(&fp);
-}
-
-void substring(char s[], char d[], int pos, int len) {
-   int c = 0;
-
-   while (c < len) {
-      d[c] = s[pos+c-1];
-      c++;
-   }
-   d[c] = '\0';
 }
